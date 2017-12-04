e49e29f970ff70801e8f11b632b7b6eb14980050
DESKTOP-O18AE16\Joanes <joanes.plazaola@alumni.mondragon.edu>
2017-12-04 11:44:23 +0100

    Added enum for packet types in protocol properties, CRC8 implemented and NodeLogic ACK started

--- lib/src/main/java/protocol/NodeLogic.java
+++ lib/src/main/java/protocol/NodeLogic.java
@@ -1,7 +1,34 @@
 package protocol;
 
+import helpers.CRC8;
+import models.Frame;
+import models.Header;
+
+import static protocol.ProtocolProperties.COUNTER;
+import static protocol.ProtocolProperties.PACKET_TYPE;
+import static protocol.ProtocolProperties.START_FRAME;
+import static protocol.ProtocolProperties.PacketType;
+
 public class NodeLogic {
-	
-	
+
+
+    public Frame sendAcknowledgment(Frame frame, String byteString, String dest, String origin) {
+        CRC8 crc8 = new CRC8();
+        crc8.reset();
+        crc8.update("000".getBytes());
+
+        Header header = new Header();
+        header.setStartFrame(byteString.substring(0, START_FRAME - 1));
+        header.setPacketType(PacketType.ACK.toString());
+        header.setCounter(byteString.substring(PACKET_TYPE, COUNTER - 1));
+        frame.setLength("00000000");
+        frame.setHeader(header);
+        frame.setDestinationId(dest);
+        //frame.getOriginId(origin);
+        // TODO Checksum function to call
+        frame.setChecksum("" + crc8.getValue());
+
+        return frame;
+    }
 
 }
--- lib/src/main/java/protocol/ProtocolProperties.java
+++ lib/src/main/java/protocol/ProtocolProperties.java
@@ -2,15 +2,38 @@ package protocol;
 
 public final class ProtocolProperties {
 
-	public static final int START_FRAME = 3;
-	public static final int PACKET_TYPE = 2;
-	public static final int COUNTER = 3;
-	public static final int ORIGIN_ID = 8;
-	public static final int DESTINATION_ID = 8;
-	public static final int LENGTH = 8;
-	public static final int CHECKSUM = 8;
-	public static final String DISCOVERY = "00";
-	public static final String ACK = "01";
-	public static final String DATA = "10";
-	public static final String MASTER_ID = "00000000";
+    public static final int START_FRAME = 3;
+    public static final int PACKET_TYPE = 2;
+    public static final int COUNTER = 3;
+    public static final int ORIGIN_ID = 8;
+    public static final int DESTINATION_ID = 8;
+    public static final int LENGTH = 8;
+    public static final int CHECKSUM = 8;
+
+    public enum PacketType {
+        DISCOVERY {
+            @Override
+            public String toString() {
+                return "";
+            }
+        },
+        ACK {
+            @Override
+            public String toString() {
+                return "bronze";
+            }
+        },
+        DATA {
+            @Override
+            public String toString() {
+                return "silver";
+            }
+        }
+    }
+    public static final String MASTER_ID = "00000000";
+
+    // TODO This have to been deleted
+    public static final String DISCOVERY = "00";
+    public static final String ACK = "01";
+    public static final String DATA = "10";
 }
