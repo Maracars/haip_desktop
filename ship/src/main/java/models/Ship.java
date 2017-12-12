1ec736d8dbe9283aba262cd2e5bfb4396177d255
DESKTOP-O18AE16\Joanes <joanes.plazaola@alumni.mondragon.edu>
2017-12-12 11:15:14 +0100

    getters and setters created for ship

--- ship/src/main/java/models/Ship.java
+++ ship/src/main/java/models/Ship.java
@@ -7,6 +7,29 @@ public class Ship {
 	private String id;
 	private Status status;
 	private List<String> actionList;
-	
 
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public List<String> getActionList() {
+		return actionList;
+	}
+
+	public void setActionList(List<String> actionList) {
+		this.actionList = actionList;
+	}
 }
