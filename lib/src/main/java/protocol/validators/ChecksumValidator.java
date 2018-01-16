bb8c01073806210cdca1316031bb3bab004bc272
Mikel Amuchastegui Zubizarreta <mikel.amuchastegui@alumni.mondragon.edu>
2018-01-16 11:05:19 +0100

    Change Checksum validator

--- lib/src/main/java/protocol/validators/ChecksumValidator.java
+++ lib/src/main/java/protocol/validators/ChecksumValidator.java
@@ -10,8 +10,12 @@ public class ChecksumValidator implements Validator {
 	@Override
 	public boolean validate(Frame frame) {
 		Integer length = (Integer.parseInt(frame.getHeader().getLength(), 2) * 8);
-		String expected = CRC8.toCRC8(frame.toString().substring(0, HEADER + ORIGIN_ID + DESTINATION_ID + length));
-
+		String expected = null;
+		try {
+			expected = CRC8.toCRC8(frame.toString().substring(0, HEADER + ORIGIN_ID + DESTINATION_ID + length));
+		}catch(StringIndexOutOfBoundsException e) {
+			return false;
+		}
 		return expected.equals(frame.getChecksum());
 	}
 }
