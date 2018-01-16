a93988683684dfc715ce6368d6391d41376e292f
Mikel Amuchastegui Zubizarreta <mikel.amuchastegui@alumni.mondragon.edu>
2018-01-16 10:00:56 +0100

    Data Type null changed.

--- lib/src/main/java/serial/Serial.java
+++ lib/src/main/java/serial/Serial.java
@@ -85,6 +85,7 @@ public class Serial extends Observable implements SerialPortEventListener {
 	}
 
 	public void sendToParser(String string) {
+		System.out.println(string);
 		boolean validFrame = FrameParser.parseRx(string);
 		if (validFrame) {
 			notifyFrame(FrameParser.getFrame());
